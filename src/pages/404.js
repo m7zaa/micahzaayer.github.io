6646dad13f406262bf0e623ee140d0e1b58ed9d4
Micah Zaayer <micahzaayer@gmail.com>
2022-01-26 10:53:11 -0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,7 +12,7 @@ const NotFoundPage = () => (
       <section id="one">
         <div className="inner">
           <h1>NOT FOUND</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <p>You just hit a route that doesn&#39;t exist... the sadness...</p>
         </div>
       </section>
     </div>
